b59a514c668b65a59d8d0f0ad22f9256839f3924
MassonMassoniy <118527160+MassonMassoniy@users.noreply.github.com>
2025-01-17 16:25:03 -0600

    Added custom dot support (.png preferred) + 8 yellow default dots

--- AimDot.py
+++ AimDot.py
@@ -1,6 +1,6 @@
 #created by Masson Massoniy
 
-from tkinter import Tk, Canvas
+from tkinter import Tk, Canvas, PhotoImage
 
 screen = Tk()
 screen.title("Aim Dot")
@@ -9,14 +9,18 @@ screen.attributes("-fullscreen", True)
 screen.attributes("-topmost", True)
 screen.resizable(False, False)
 
-screen.configure(bg= "#000000")
-screen.wm_attributes("-transparentcolor", "#000000")
+screen.configure(bg= "#000001")
+screen.wm_attributes("-transparentcolor", "#000001")
 
 screen_width = screen.winfo_screenwidth()
 screen_height = screen.winfo_screenheight()
 dot_radius = screen_width * 0.001
 
-dot = Canvas(screen, height= 20, width= 20, bg= "#FFFF00", highlightthickness=0)
-dot.place(relx= 0.5, rely= 0.5, relheight= 0.002, relwidth= 0.001, anchor= "center")
+dot_image = PhotoImage(file= "dots/x12Dot_Tp_Y.png")
+
+canvas = Canvas(screen, height= dot_image.height(), width= dot_image.width(), bg= "#000001", highlightthickness=0)
+canvas.place(relx= 0.5, rely= 0.5, anchor= "center")
+
+canvas.create_image(dot_image.width() / 2, dot_image.height() / 2, image= dot_image, anchor= "center")
 
 screen.mainloop()
